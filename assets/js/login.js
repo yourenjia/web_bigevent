14abd92e1158ceb04f892a5782fd1e56d5097f66
itheima <itheima@itcast.cn>
2022-02-22 15:12:27 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+$(function() {
+    // 点击“去注册账号”的链接
+    $('#link_reg').on('click', function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击“去登录”的链接
+    $('#link_login').on('click', function() {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    var form = layui.form;
+    var layer = layui.layer;
+    form.verify({
+        // 自定义了一个叫做 pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 校验两次密码是否一致的规则
+        repwd: function(value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+
+    $("#form_reg").on('submit', function(e) {
+        e.preventDefault();
+        var data = {
+            username: $("#form_reg [name=username]").val(),
+            password: $("#form_reg [name=password]").val()
+        }
+        $.post('/api/reguser', data,
+            function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg(res.message);
+                $("#link_login").click();
+            })
+    })
+
+    $("#form_login").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "/api/login",
+            method: "post",
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    console.log(res);
+                    return layer.msg(res.message);
+                }
+                localStorage.setItem('token', res.token);
+                layer.msg('登录成功');
+                location.href = '/index.html'
+            }
+        })
+    })
+})
